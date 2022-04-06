13cf230e6a9faead874cc8bb79110d5c93f0033b
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-04-06 08:42:11 -0300

    SRP na prática

---
+++ 0x01/srp/CustomerService.java
@@ -0,0 +1,16 @@
+public class CustomerService {
+
+    public void ListCustomer(){
+    }
+
+    public void addCustomer(){
+    }
+
+    public void updateCustomer(){
+    }
+
+    public void deleteCustomer(){
+    }
+
+
+}
