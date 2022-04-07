3fa2c0c43ef934f452c882859e497079da7f2f74
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-04-07 11:41:17 -0300

    Solid:  ISP

---
+++ 0x02/isp/Car.java
@@ -0,0 +1,11 @@
+public class Car implements IMotorVehicle, IVehicleCar{
+    @Override
+    public void configureCar(String color, String year, double engine, int seats) {
+
+    }
+
+    @Override
+    public void kmPerLitre() {
+
+    }
+}
