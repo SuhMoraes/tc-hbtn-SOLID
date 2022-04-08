89f55bd19c35f477040fddf1588519851e4fd0ab
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-04-08 08:23:53 -0300

    Clean Code: Functions

---
+++ 0x03/fuction/functions.java
@@ -0,0 +1,45 @@
+public class functions {
+    // Faça somente 1 coisa
+    //B
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    //Somente 1 nível de abstração
+    //A
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.save(user);
+    }
+
+
+    // Arguments
+    //A
+    public void saveUser(User user){};
+
+    //  No side effects
+    public User saveUser(User user){
+        // if statement should be avoided, why saveUser action is changing role of a user?
+        if(user.isAdmin){
+            user.setRole(user);
+        }
+        return userRepository.save(user);
+    }
+
+    // Evite duplicados, extraindo para novos métodos
+
+    //B
+    public Environment getEnvironment(){
+        return environmentService.getEnvironment(getVersion());
+    }
+    public Release getRelease(){
+        return releaseService.getRelease(getVersion());
+    }
+    private String getVersion(){
+        return versionService.getVersion();
+    }
+
+}
