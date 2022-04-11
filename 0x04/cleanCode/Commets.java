786c2aeb565480a1e04b2d22263c0eb05c7f314f
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-04-11 14:36:09 -0300

    Comentarios

---
+++ 0x04/cleanCode/Commets.java
@@ -0,0 +1,23 @@
+public class Comments {
+
+    public Owner getTestcaseOwner(Testcase testcase){};
+
+    public Hours getHoursInQuarter(){};
+
+    // TODO Este métod deve ser removido
+    public void check(){
+        return;
+    }
+
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+
+    }
+}
+
